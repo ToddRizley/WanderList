3e67746052a22a1c566d79a64af695321b98b456
ToddRizley <todd.rizley@gmail.com>
2016-07-13 09:54:30 -0400

    add user_cities join table fix user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,8 @@ class User < ApplicationRecord
   
   has_secure_password
 
-  belongs_to :city
+  has_many :cities
   has_many :itineraries
   has_many :flights, through: :itineraries
 
-  validates :city_id, presence: false
-  validates :password, presence: true
 end
