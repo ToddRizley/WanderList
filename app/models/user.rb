15509d369506121998506584ffd8af98de4609e6
ToddRizley <todd.rizley@gmail.com>
2016-07-13 00:27:08 -0400

    added some validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   
   has_secure_password
 
-  belongs_to :city 
+  belongs_to :city
   has_many :itineraries
   has_many :flights, through: :itineraries
 
-
+  validates :city_id, presence: false
+  validates :password, presence: true
 end
