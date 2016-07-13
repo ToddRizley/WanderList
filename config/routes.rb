c0173d1ec3abbe3a013ddeb026f6aebddbdfe00f
ToddRizley <todd.rizley@gmail.com>
2016-07-12 20:48:37 -0400

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   
   #root
-  root "static_pages#home"
+  root to: "sessions#home"
+
+
 
   # sessions
   get "/signin", to: "sessions#new"
   post "/sessions/create", to: "sessions#create"
   delete "/signout", to: "sessions#destroy"
 
-  resources :users, :cities
+  resources :users, :cities, :sessions, 
 
 
 
