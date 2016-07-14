158a0a51ec29bf468fb7b05114af3eab9ca7c587
ToddRizley <todd.rizley@gmail.com>
2016-07-14 19:54:33 -0400

    fix errors on sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,21 +7,15 @@ class User < ApplicationRecord
   has_many :itineraries
   has_many :flights, through: :itineraries
 
+   validates :name, presence: { message: "must exist" }, uniqueness: { message: "already exists"}
 
-  def city
-    
-  end
+   validates :password, length: { minimum: 4 }
+   validate :name_is_alpha_num?
 
-  # validates :name, presence: { message: "Name must exist" }, uniqueness: { message: "Name already exists"}
-
-  # validates :password, length: { minimum: 4 }
-
-  # validates :budget, numericality: { message: "Budget must be a number" }, numericality: { greater_than: 0}
-
-  # validate :return_after_departure
-
-  def alpha?
-    !!match(/^[[:alnum:]]+$/)
+  def name_is_alpha_num?
+    if !self.name.match(/^[[:alnum:]]+$/)
+      errors.add(:name, "may only be alphanumeric characters")
+    end
   end
 
    #def return_after_departure
@@ -29,6 +23,10 @@ class User < ApplicationRecord
   #     errors.add(:return, "must be after departure")
   #   end
   # end
+
+  # validates :budget, numericality: { message: "Budget must be a number" }, numericality: { greater_than: 0}
+
+  # validate :return_after_departure
 
  
 
