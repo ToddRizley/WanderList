d2b3dbfc6b2bbca3bc55a8718a4a87f37f34df1b
ToddRizley <todd.rizley@gmail.com>
2016-07-14 11:32:22 -0400

    add gems airport test, delete files

--- test/models/airport_test.rb
+++ test/models/airport_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
-class AirportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+Rspec.describe Airport, :type => :model do 
+  let(:city) {
+    City.create(
+      :name => "New York City",
+      :description => "concrete bunghole where dreams are made up"
+    )
+  }
+
+let(:airport) {
+  Airport.create(name: "LaGuardia", city_id: city.id)
+
+}
+
+  it "it is valid with a city_id and a name" do
+      expect(airport).to be_valid
+  end
+
+  it "belongs to one city" do
+    expect(airport.city).to eq(city)
+  end
+
+
+  ##insert test methods
+
 end
