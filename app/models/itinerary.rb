01da48efb9239e6000d969d5200312cdb649cf84
eaharonoff <emanuel.aharonoff@gmail.com>
2016-07-18 14:15:07 -0400

    style additions to search_results

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -2,6 +2,27 @@ class Itinerary < ApplicationRecord
   belongs_to :departing_flight, :class_name => "Flight"
   belongs_to :return_flight, :class_name => "Flight"
   belongs_to :user
-end
 
+
+  def total_price
+    self.departing_flight.price.to_i + self.return_flight.price.to_i
+  end
+
+  def departure_city
+    self.departing_flight.departure_city.name
+  end
+
+  def arrival_city
+    self.return_flight.departure_city.name
+  end
+
+  def departure_date
+    self.departing_flight.departure_date
+  end
+
+  def return_date  
+    self.return_flight.arrival_date
+  end
+
+end
 
