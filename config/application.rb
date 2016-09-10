ec9e58df0ed0682f381af93caa7684378d67d3bd
ToddRizley <todd.rizley@gmail.com>
2016-09-10 15:22:48 -0400

    precompile fix

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,14 @@
 require_relative 'boot'
 
 require 'rails/all'
-config.assets.initialize_on_precompile = true
-config.serve_static_assets = true
-# config.serve_static_assets = true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 module FlatironProject1
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = true
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
