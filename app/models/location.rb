a2245b628ce882c44fa4b8ce9b538e0167009f54
eaharonoff <emanuel.aharonoff@gmail.com>
2016-09-16 12:35:58 -0400

    moved #prepare_quotes to service object quote_updater

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,41 +1,4 @@
 class Location < ApplicationRecord
   has_many :airports
   validates :city_ref, uniqueness: true
-
-  def self.check_db_for_location(quote, leg, placeid, dir_city, dir_country)
-      location = Location.find_by(city_ref: quote[leg][placeid])
-      quote[leg][dir_city] = location.city_name
-      quote[leg][dir_country] = location.country_name
-      quote
-  end
-
-  def self.parse_location_data(quote, places, placeid, airport, leg, dir_city, dir_country)  
-    city_object = search(quote,places,leg, placeid, "PlaceId")
-    location = Location.create(city_ref: city_object["PlaceId"], city_name: city_object["CityName"], country_name: city_object["CountryName"])
-    airport = Airport.find_or_create_by(name: city_object["Name"], location_id: location.id)
-    quote[leg][dir_city] = location.city_name
-    quote[leg][airport] = airport.name
-    quote[leg][dir_country] = location.country_name
-    quote
-  end
-
-  def self.search(quote, array, leg, originid, placeid)
-      midpoint = (array.length/2)
-      lower = array.slice(0..midpoint)
-      higher = array.slice(midpoint..array.length)
-      length = array.length 
-      if (length == 1) 
-        array[0]
-      elsif (quote[leg][originid] > array[midpoint][placeid])
-
-        search(quote,higher,leg, originid, placeid)
-      elsif(quote[leg][originid] < array[midpoint][placeid])
-
-        search(quote,lower,leg,originid, placeid)
-      else
-
-        array[midpoint]
-      end
-  end
-
 end
--- app/models/services/quote_updater.rb
+++ app/models/services/quote_updater.rb
@@ -3,29 +3,67 @@ module Services
     def prepare_quotes(quotes, carriers, places)
       quotes.each do |quote|
         if Location.find_by(city_ref: quote["OutboundLeg"]["OriginId"])
-          Location.check_db_for_location(quote, "OutboundLeg", "OriginId", "OriginCity", "OriginCountry")
+          update_location(quote, "OutboundLeg", "OriginId", "OriginCity", "OriginCountry")
         else
-          Location.parse_location_data(quote, places, "OriginId", "OriginAirport", "OutboundLeg", "OriginCity", "OriginCountry" )
+          create_and_update_location(quote, places, "OriginId", "OriginAirport", "OutboundLeg", "OriginCity", "OriginCountry" )
         end
 
         if Location.find_by(city_ref: quote["OutboundLeg"]["DestinationId"])
-          Location.check_db_for_location(quote, "OutboundLeg", "DestinationId", "DestinationCity", "DestinationCountry")
+          update_location(quote, "OutboundLeg", "DestinationId", "DestinationCity", "DestinationCountry")
         else
-          Location.parse_location_data(quote, places, "DestinationId", "DestinationAirport", "OutboundLeg", "DestinationCity", "DestinationCountry" )
+          create_and_update_location(quote, places, "DestinationId", "DestinationAirport", "OutboundLeg", "DestinationCity", "DestinationCountry" )
         end
 
         if Location.find_by(city_ref: quote["InboundLeg"]["OriginId"])
-          Location.check_db_for_location(quote, "InboundLeg", "OriginId", "OriginCity", "OriginCountry")
+          update_location(quote, "InboundLeg", "OriginId", "OriginCity", "OriginCountry")
         else
-          Location.parse_location_data(quote, places, "OriginId", "OriginAirport", "InboundLeg", "OriginCity", "OriginCountry" )
+          create_and_update_location(quote, places, "OriginId", "OriginAirport", "InboundLeg", "OriginCity", "OriginCountry" )
         end
 
         if Location.find_by(city_ref: quote["InboundLeg"]["DestinationId"])
-          Location.check_db_for_location(quote, "InboundLeg", "DestinationId", "DestinationCity", "DestinationCountry")
+          update_location(quote, "InboundLeg", "DestinationId", "DestinationCity", "DestinationCountry")
         else
-          Location.parse_location_data(quote, places, "DestinationId", "DestinationAirport", "InboundLeg", "DestinationCity", "DestinationCountry" )
+          create_and_update_location(quote, places, "DestinationId", "DestinationAirport", "InboundLeg", "DestinationCity", "DestinationCountry" )
         end
         Airline.match_airlines_in_quotes(quote, carriers)
+      end
+    end
+
+    
+    private 
+    def update_location(quote, leg, placeid, dir_city, dir_country)
+      location = Location.find_by(city_ref: quote[leg][placeid])
+      quote[leg][dir_city] = location.city_name
+      quote[leg][dir_country] = location.country_name
+      quote
+    end
+
+    def create_and_update_location(quote, places, placeid, airport, leg, dir_city, dir_country)  
+      city_object = search(quote,places,leg, placeid, "PlaceId")
+      location = Location.create(city_ref: city_object["PlaceId"], city_name: city_object["CityName"], country_name: city_object["CountryName"])
+      airport = Airport.find_or_create_by(name: city_object["Name"], location_id: location.id)
+      quote[leg][dir_city] = location.city_name
+      quote[leg][airport] = airport.name
+      quote[leg][dir_country] = location.country_name
+      quote
+    end
+
+    def search(quote, array, leg, originid, placeid)
+      midpoint = (array.length/2)
+      lower = array.slice(0..midpoint)
+      higher = array.slice(midpoint..array.length)
+      length = array.length 
+      if (length == 1) 
+        array[0]
+      elsif (quote[leg][originid] > array[midpoint][placeid])
+
+        search(quote,higher,leg, originid, placeid)
+      elsif(quote[leg][originid] < array[midpoint][placeid])
+
+        search(quote,lower,leg,originid, placeid)
+      else
+
+        array[midpoint]
       end
     end
   end
