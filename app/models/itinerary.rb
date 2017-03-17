789594ce4233df9641fc3598b4a778a7b6a845a4
ToddRizley <todd.rizley@gmail.com>
2017-03-17 12:52:03 -0400

    refactor some service objects

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,5 +1,4 @@
 class Itinerary < ApplicationRecord
   belongs_to :user
   belongs_to :quote
-  
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,4 @@
 class Quote < ApplicationRecord
-
   has_many :itineraries
   has_many :users, through: :itineraries
 end
