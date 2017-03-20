a32a7792c8ae4d532314aa0f268cf0aff8568754
ToddRizley <todd.rizley@gmail.com>
2017-03-19 20:30:28 -0400

    update optimization of queries

--- app/models/services/quote_updater.rb
+++ app/models/services/quote_updater.rb
@@ -2,9 +2,10 @@ module Services
   class QuoteUpdater
     def prepare_quotes(quotes, carriers, places)
       city_refs = Location.pluck(:city_ref)
+      airport_refs = Airport.pluck(:airport_ref)
       quotes.each do |quote|
-        update_outbound(quote, places, city_refs)
-        update_inbound(quote, places, city_refs)
+        update_outbound(quote, places, city_refs, airport_refs)
+        update_inbound(quote, places, city_refs, airport_refs)
         update_airline(quote, carriers)
       end
     end
@@ -15,12 +16,12 @@ module Services
       Airline.match_airlines_in_quotes(quote, carriers)
     end
 
-    def update_inbound(quote, places, city_refs)
-      update_inbound_origin(quote, places, city_refs)
-      update_inbound_destination(quote, places, city_refs)
+    def update_inbound(quote, places, city_refs, airport_refs)
+      update_inbound_origin(quote, places, city_refs, airport_refs)
+      update_inbound_destination(quote, places, city_refs, airport_refs)
     end
 
-    def update_inbound_origin(quote, places, city_refs)
+    def update_inbound_origin(quote, places, city_refs, airport_refs)
       if city_refs.include?(quote['InboundLeg']['OriginId'])
         update_location(quote,
                         'InboundLeg',
@@ -28,36 +29,36 @@ module Services
                         'OriginCity',
                         'OriginCountry')
       else
-        create_and_update_location(quote, places, 'OriginId', 'OriginAirport', 'InboundLeg', 'OriginCity', 'OriginCountry')
+        create_and_update_location(quote, places, 'OriginId', 'OriginAirport', 'InboundLeg', 'OriginCity', 'OriginCountry', airport_refs)
       end
     end
 
-    def update_inbound_destination(quote, places, city_refs)
+    def update_inbound_destination(quote, places, city_refs, airport_refs)
       if city_refs.include?(quote['InboundLeg']['DestinationId'])
         update_location(quote, 'InboundLeg', 'DestinationId', 'DestinationCity', 'DestinationCountry')
       else
-        create_and_update_location(quote, places, 'DestinationId', 'DestinationAirport', 'InboundLeg', 'DestinationCity', 'DestinationCountry')
+        create_and_update_location(quote, places, 'DestinationId', 'DestinationAirport', 'InboundLeg', 'DestinationCity', 'DestinationCountry', airport_refs)
       end
     end
 
-    def update_outbound(quote, places, city_refs)
-      update_outbound_origin(quote, places, city_refs)
-      update_outbound_destination(quote, places, city_refs)
+    def update_outbound(quote, places, city_refs, airport_refs)
+      update_outbound_origin(quote, places, city_refs, airport_refs)
+      update_outbound_destination(quote, places, city_refs, airport_refs)
     end
 
-    def update_outbound_origin(quote, places, city_refs)
+    def update_outbound_origin(quote, places, city_refs, airport_refs)
       if city_refs.include?(quote['OutboundLeg']['OriginId'])
         update_location(quote, 'OutboundLeg', 'OriginId', 'OriginCity', 'OriginCountry')
       else
-        create_and_update_location(quote, places, 'OriginId', 'OriginAirport', 'OutboundLeg', 'OriginCity', 'OriginCountry')
+        create_and_update_location(quote, places, 'OriginId', 'OriginAirport', 'OutboundLeg', 'OriginCity', 'OriginCountry', airport_refs)
       end
     end
 
-    def update_outbound_destination(quote, places, city_refs)
+    def update_outbound_destination(quote, places, city_refs, airport_refs)
       if city_refs.include?(quote['OutboundLeg']['DestinationId'])
         update_location(quote, 'OutboundLeg', 'DestinationId', 'DestinationCity', 'DestinationCountry')
       else
-        create_and_update_location(quote, places, 'DestinationId', 'DestinationAirport', 'OutboundLeg', 'DestinationCity', 'DestinationCountry')
+        create_and_update_location(quote, places, 'DestinationId', 'DestinationAirport', 'OutboundLeg', 'DestinationCity', 'DestinationCountry', airport_refs)
       end
     end
 
@@ -68,10 +69,10 @@ module Services
       quote
     end
 
-    def create_and_update_location(quote, places, placeid, airport, leg, dir_city, dir_country)
+    def create_and_update_location(quote, places, placeid, airport, leg, dir_city, dir_country, airport_refs)
       city_object = binary_search_for_city(quote, places, leg, placeid, 'PlaceId')
       location = Location.create(city_ref: city_object['PlaceId'], city_name: city_object['CityName'], country_name: city_object['CountryName'])
-      airport_update = Airport.find_or_create_by(name: city_object['Name'], location_id: location.id)
+      airport_update = Airport.create(name: city_object['Name'], location_id: location.id, airport_ref: city_object['PlaceId']) unless airport_refs.include?(city_object['PlaceId'])
       quote[leg][dir_city] = location.city_name
       quote[leg][airport] = airport_update.name
       quote[leg][dir_country] = location.country_name
--- db/schema.rb
+++ db/schema.rb
@@ -10,58 +10,60 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_160_907_181_320) do
+ActiveRecord::Schema.define(version: 20160907181320) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'airlines', force: :cascade do |t|
-    t.string  'name'
-    t.integer 'carrier_ref'
+  create_table "airlines", force: :cascade do |t|
+    t.string  "name"
+    t.integer "carrier_ref"
   end
 
-  create_table 'airports', force: :cascade do |t|
-    t.string   'name'
-    t.integer  'airport_ref'
-    t.integer  'location_id'
-    t.datetime 'created_at',  null: false
-    t.datetime 'updated_at',  null: false
+  create_table "airports", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "airport_ref"
+    t.integer  "location_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table 'itineraries', force: :cascade do |t|
-    t.integer  'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer  'quote_id'
+  create_table "itineraries", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "quote_id"
   end
 
-  create_table 'locations', force: :cascade do |t|
-    t.string   'city_name'
-    t.integer  'city_ref'
-    t.string   'country_name'
-    t.integer  'country_ref'
-    t.datetime 'created_at',   null: false
-    t.datetime 'updated_at',   null: false
+  create_table "locations", force: :cascade do |t|
+    t.string   "city_name"
+    t.integer  "city_ref"
+    t.string   "country_name"
+    t.integer  "country_ref"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
-  create_table 'quotes', force: :cascade do |t|
-    t.string   'destination'
-    t.string   'destination_country'
-    t.date     'departure_date'
-    t.string   'outbound_carrier'
-    t.date     'return_date'
-    t.string   'return_carrier'
-    t.float    'price'
-    t.datetime 'created_at',          null: false
-    t.datetime 'updated_at',          null: false
+  create_table "quotes", force: :cascade do |t|
+    t.string   "destination"
+    t.string   "destination_country"
+    t.date     "departure_date"
+    t.string   "outbound_carrier"
+    t.date     "return_date"
+    t.string   "return_carrier"
+    t.float    "price"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string   'name'
-    t.integer  'budget'
-    t.date     'departure_flight'
-    t.date     'return_flight'
-    t.datetime 'created_at',       null: false
-    t.datetime 'updated_at',       null: false
-    t.string   'password_digest'
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "budget"
+    t.date     "departure_flight"
+    t.date     "return_flight"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "password_digest"
   end
+
 end
