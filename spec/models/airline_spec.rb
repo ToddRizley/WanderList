413653f072682eb6d921138a20d619c9370072bf
ToddRizley <todd.rizley@gmail.com>
2017-03-20 13:10:38 -0400

    finish test coverage for models

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -23,27 +23,34 @@ describe Airline do
     expect(airline2.errors.messages.keys.include?(:carrier_ref)).to eq(true)
     expect(airline2.errors.messages[:carrier_ref].include?('has already been taken')).to eq(true)
   end
+
+  describe 'class methods' do
+    before(:each) do
+      @quote_before = { 'OutboundLeg' => { 'CarrierIds' => [1161] }, 'InboundLeg' => { 'CarrierIds' => [1218] } }
+      @carriers = [{ 'CarrierId' => 1161, 'Name' => 'Pegasus Airlines' }, { 'CarrierId' => 1218, 'Name' => 'Iberia' }]
+      @leg1 = 'OutboundLeg'
+      @leg2 = 'InboundLeg'
+      @carrier1 = { 'CarrierId' => 7, 'Name' => 'Vueling Airlines' }
+      @carrier2 = { 'CarrierId' => 1161, 'Name' => 'Pegasus Airlines' }
+      @carrier3 = { 'CarrierId' => 1218, 'Name' => 'Iberia' }
+    end
+    it '#match_airlines_in_quotes' do
+      quote_after = Airline.match_airlines_in_quotes(@quote_before, @carriers)
+      expect(quote_after['OutboundLeg']['Carriers'].include?('Pegasus Airlines')).to eq(true)
+      expect(quote_after['InboundLeg']['Carriers'].include?('Iberia')).to eq(true)
+      expect(Airline.where(name: 'Pegasus Airlines')).to exist
+      expect(Airline.where(name: 'Iberia')).to exist
+    end
+
+    it '#edit_quote_carriers to update with correct carrier' do
+      new_quote = Airline.edit_quote_carriers(@quote_before, 'InboundLeg', @carrier3)
+      expect(new_quote['InboundLeg'].keys.include?('Carriers')).to be(true)
+      expect(new_quote['InboundLeg']['Carriers'].include?('Iberia')).to be(true)
+    end
+
+    it '#edit_quote_carriers to not update with wrong carrier' do
+      new_quote = Airline.edit_quote_carriers(@quote_before, 'InboundLeg', @carrier1)
+      expect(new_quote['InboundLeg'].keys.include?('Carriers')).to be(false)
+    end
+  end
 end
-#   validates :name
-#   validates :carrier_ref
-#
-#   def self.match_airlines_in_quotes(quote, carriers)
-#     airline_ref_numbers = Airline.pluck(:carrier_ref)
-#     carriers.each do |carrier|
-#       Airline.create(name: carrier['Name'], carrier_ref: carrier['CarrierId']) unless airline_ref_numbers.include?(carrier['CarrierId'])
-#       Airline.edit_quote_carriers(quote, 'OutboundLeg', carrier)
-#       Airline.edit_quote_carriers(quote, 'InboundLeg', carrier)
-#     end
-#     quote
-#   end
-#
-#   def self.edit_quote_carriers(quote, leg, carrier)
-#     quote[leg]['CarrierIds'].each do |car|
-#       if car == carrier['CarrierId']
-#         quote[leg]['Carriers'] ||= []
-#         quote[leg]['Carriers'] << carrier['Name']
-#       end
-#     end
-#     quote
-#   end
-# end
